32e360250812a7a9a0f582961510ef4b1aae1335
David <3.hayem.es@gmail.com>
2021-08-11 19:38:20 +0400

    creat new script

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,16 @@
-/* const answers = [];
-answers[0] = prompt('What is your name?', '');
-answers[1] = prompt('What is your surname?', '');
-answers[2] = +prompt('How old are you?', '');
-
-console.log(answers); */
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+const
+  a = prompt('Один из последних просмотренных фильмов?', ''),
+  b = prompt('На сколько оцените его?', ''),
+  c = prompt('Один из последних просмотренных фильмов?', ''),
+  d = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
